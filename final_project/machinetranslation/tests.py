9dcb075ec301989353fb046979c380c940adf8bf
u87ypopov <87y.popov@gmail.com>
2023-04-09 03:33:01 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,16 +2,25 @@ import unittest
 
 from translator import english_to_french, french_to_english
 
-class TestEnglishToFrench(unittest.TestCase): 
- def test1(self): 
-   
-      # Test Bonjour returns Hello
+class TestE2F(unittest.TestCase):
 
-     self.assertEqual(english_to_french('Hello'), 'Bonjour')
+#"""English to French tests"""
 
-        # Test Bonjour does not return Bonjour
+ def test1(self):
 
-     self.assertNotEqual(english_to_french('Hello'), 'Hello')
-     
- unittest.main()
+# Test Hello returns Bonjour
 
+      self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+# Test Hello does not return Hello
+
+      self.assertNotEqual(english_to_french('Hello'), 'Hello')
+
+# Test None returns empty string
+
+      self.assertNotEqual(english_to_french("None"), '')
+
+# Test empty string returns empty string
+
+      self.assertNotEqual(english_to_french(0), 0)
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,18 +3,17 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
-authenticator = IAMAuthenticator('apikey')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
-language_translator.set_service_url('url')
+
+
 load_dotenv()
 apikey= os.environ['apikey']
 
 url= os.environ['url']
-
-language_translator.set_service_url("https://api.us-south.language-translator.watson.cloud.ibm.com/")
+authenticator=IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
 
 def english_to_french(englishText):
     translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
