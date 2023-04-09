ea003daa138f873e1a853f03833877bdfdce5fcd
u87ypopov <87y.popov@gmail.com>
2023-04-09 06:42:36 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,26 +1,28 @@
+"""Testing Module"""
 import unittest
-
 from translator import english_to_french, french_to_english
-
 class TestE2F(unittest.TestCase):
-
-#"""English to French tests"""
-
- def test1(self):
-
-# Test Hello returns Bonjour
-
-      self.assertEqual(english_to_french('Hello'), 'Bonjour')
-
-# Test Hello does not return Hello
-
-      self.assertNotEqual(english_to_french('Hello'), 'Hello')
-
-# Test None returns empty string
-
-      self.assertNotEqual(english_to_french("None"), '')
-
-# Test empty string returns empty string
-
-      self.assertNotEqual(english_to_french(0), 0)
+    """English to French tests"""
+    def test1(self):
+        """English to French tests"""
+ #"""Test Hello returns Bonjour"""
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+ # Test Hello does not return Hello
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
+ # Test None returns empty string
+        self.assertNotEqual(english_to_french("None"), '')
+ # Test empty string returns empty string
+        self.assertNotEqual(english_to_french(0), 0)
+class TestF2E(unittest.TestCase):
+    """French to English tests"""
+    def test2(self):
+        """French to English tests"""
+ # Test Bonjour returns Hello
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+ # Test Bonjour does not return Bonjour
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
+ # Test None returns empty string
+        self.assertNotEqual(french_to_english("None"), '')
+ # Test empty string returns empty string
+        self.assertNotEqual(french_to_english(0), 0)
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,29 +1,23 @@
-import json
+"""Translator"""
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
-
-
 load_dotenv()
 apikey= os.environ['apikey']
-
 url= os.environ['url']
 authenticator=IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
-
-def english_to_french(englishText):
-    translation = language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
-
-def french_to_english(frenchText):
-    translation = language_translator.translate(text=frenchText, model_id="fr-en").get_result()
-    englishText = translation["translations"][0]["translation"]
-
-    return englishText
-
-
+def english_to_french(english_text):
+    """Translate from English to French"""
+    translation = language_translator.translate(text=english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+def french_to_english(french_text):
+    """Translate from French to English"""
+    translation = language_translator.translate(text=french_text, model_id="fr-en").get_result()
+    english_text = translation["translations"][0]["translation"]
+    return english_text
